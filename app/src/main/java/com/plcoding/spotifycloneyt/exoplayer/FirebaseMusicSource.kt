1b3c0cda294f73ba8866ad793038d8841a957c16
seanlau <lthon0225@gmail.com>
2023-11-21 18:28:47 +0800

    add media concat and add media meta data

--- app/src/main/java/com/plcoding/spotifycloneyt/exoplayer/FirebaseMusicSource.kt
+++ app/src/main/java/com/plcoding/spotifycloneyt/exoplayer/FirebaseMusicSource.kt
@@ -1,8 +1,55 @@
 package com.plcoding.spotifycloneyt.exoplayer
 
+import android.support.v4.media.MediaMetadataCompat
+import android.support.v4.media.MediaMetadataCompat.*
+import android.support.v4.media.session.MediaSessionCompat
+import androidx.core.net.toUri
+import com.google.android.exoplayer2.source.ConcatenatingMediaSource
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.plcoding.spotifycloneyt.data.remote.MusicDatabase
 import com.plcoding.spotifycloneyt.exoplayer.State.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
 
-class FirebaseMusicSource {
+class FirebaseMusicSource @Inject constructor(
+    private val musicDatabase: MusicDatabase
+){
+
+    private var songs = emptyList<MediaMetadataCompat>()
+
+    suspend fun fetchMediaData()= withContext(Dispatchers.IO){
+        state = STATE_INITIALIZING
+        val allSongs = musicDatabase.getAllSong()
+        songs = allSongs.map { song ->
+            MediaMetadataCompat.Builder()
+                .putString(METADATA_KEY_ARTIST, song.subTitle)
+                .putString(METADATA_KEY_MEDIA_ID, song.mediaId)
+                .putString(METADATA_KEY_TITLE, song.title)
+                .putString(METADATA_KEY_DISPLAY_TITLE, song.title)
+                .putString(METADATA_KEY_DISPLAY_ICON_URI, song.imageUrl)
+                .putString(METADATA_KEY_MEDIA_URI, song.songUrl)
+                .putString(METADATA_KEY_ALBUM_ART_URI, song.imageUrl)
+                .putString(METADATA_KEY_DISPLAY_SUBTITLE, song.subTitle)
+                .putString(METADATA_KEY_DISPLAY_DESCRIPTION, song.subTitle)
+                .build()
+        }
+        state = STATE_INITIALIZED
+    }
+
+    fun asMediaSource(dataSourceFactory: DefaultDataSourceFactory): ConcatenatingMediaSource {
+        val concatenatingMediaSource = ConcatenatingMediaSource()
+        songs.forEach { song ->
+            val mediaSource = ProgressiveMediaSource.Factory(dataSourceFactory)
+                .createMediaSource(song.getString(METADATA_KEY_MEDIA_URI).toUri())
+            concatenatingMediaSource.addMediaSource((mediaSource))
+        }
+        return concatenatingMediaSource
+    }
+
+    
+
     private val onReadyListeners = mutableListOf<(Boolean) -> Unit>()
 
     private var state: State = STATE_CREATED
@@ -10,10 +57,27 @@ class FirebaseMusicSource {
             if(value == STATE_INITIALIZED || value == STATE_ERROR) {
                 synchronized(onReadyListeners) {
                     field = value
-                    onReadyListeners.forEach {  }
+                    onReadyListeners.forEach { listener ->
+                        // state on have STATE_INITIALIZED || STATE_ERROR
+                        // therefore, if true(STATE_INITIALIZED) means OK,
+                        // false(STATE_ERROR) means there is an error.
+                        listener(state == STATE_INITIALIZED)
+                    }
                 }
+            } else {
+                field = value
             }
         }
+
+    fun whenReady(action: (Boolean) -> Unit): Boolean {
+        if (state == STATE_CREATED || state == STATE_INITIALIZING) {
+            onReadyListeners += action
+            return false
+        } else {
+            action(state == STATE_INITIALIZED)
+            return true
+        }
+    }
 
 }
 
